d82c5333f1f5028ca98bdced6eaad4de0106fa61
Wyatt Ptak <ptakwyatt@gmail.com>
2023-04-25 15:26:11 -0700

    addTask function works now

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-let list = document.getElementById("list");
+let ul = document.getElementById("list");
+
 
 document.querySelector("#addTask").onclick = function() {
     let taskName = document.getElementById("taskName").value;
-    document.createElement("li", taskName);
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(taskName));
+    ul.appendChild(li);
 }
+
+document.querySelector("#removeTask")
