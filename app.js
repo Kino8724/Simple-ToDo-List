6d6879c365d254904e38a086f53614b2e5a3b1a6
Wyatt Ptak <ptakwyatt@gmail.com>
2023-04-25 12:11:46 -0700

    Testing Clone on local machine

--- app.js
+++ app.js
@@ -2,11 +2,6 @@ let list = document.getElementById("list");
 let tasks = [];
 let taskName = String(document.getElementById("taskName"))
 
-function addTask() {
-    tasks.push(taskName)
-    tasks.forEach(task => {
-        let li = document.createElement("li");
-        li.innerHTML(task);
-        list.appendChild(li);
-    });
+function AddTask(taskName) {
+    
 }
