652637ebcef01d4d46cbb697935e1d76f9e3fe1b
Wyatt Ptak <ptakwyatt@gmail.com>
2023-04-26 15:14:55 -0700

    addTask Function changed task view

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 let ul = document.getElementById("list");
 
-
+// View for the addTask
+// ul>li>div>span+button *emmett abreviation*
 document.querySelector("#addTask").onclick = function() {
     let taskName = document.getElementById("taskName").value;
     let li = document.createElement("li");
-    li.appendChild(document.createTextNode(taskName));
+    let div = document.createElement("div");
+    let span = document.createElement("span");
+    let button = document.createElement("button");
+    span.appendChild(document.createTextNode(taskName));
+    button.appendChild(document.createTextNode("REMOVE"));
+    div.appendChild(span);
+    div.appendChild(button);
+    li.appendChild(div);
     ul.appendChild(li);
 }
 
